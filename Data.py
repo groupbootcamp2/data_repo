b97aad592aea685277c99126ecdc6d63c00346cb
user <user@DESKTOP-45L6QC9>
2022-06-29 14:41:22 +0300

    data from cifar-10 perfect

--- Data.py
+++ Data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import os
+from PIL import Image as im
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -8,23 +10,41 @@ def unpickle(file):
     return dict
 
 
-def create_df():
+def create_df_and_images(datasetType):
     df = pd.DataFrame()
-    dirname="C:\\Users\\User\\Downloads\\cifar-10-python\\cifar-10-batches-py"
+    dataset="cifar"+str(datasetType)
+    dirname="..\\cifar-10-batches-py"
     for i in range(1,6):
         file=os.path.join(dirname,"data_batch_"+str(i))
         dict=unpickle(file)
-        DICTLEN=len(dict[ b'filenames'])
-        newDict={"images":dict[ b'filenames'],"labels": dict[b'labels'] , "source_image": ["cifar10"]*DICTLEN, "batch":[i]*DICTLEN}
+        DictLen=len(dict[ b'filenames'])
+        path="..\\resources\\"
+        newDict={"image_name":dict[ b'filenames'],"image_path":[path]*DictLen, "label": dict[b'labels'] , "source_image": [dataset]*DictLen, "batch":[i]*DictLen}
+        newDict["image_name"]=[(str(sub)).replace("'", "")[1:] for sub in newDict["image_name"]]
+        print(type(str(newDict["image_name"][0])))
         newDF=pd.DataFrame(newDict)
+
+        #images
+        for arr in range(DictLen):
+            array = np.array(dict[b'data'][arr])
+            rgbArray = np.zeros((32, 32, 3), 'uint8')
+            rgbArray[..., 0] = array[:1024].reshape(32, 32)
+            rgbArray[..., 1] = array[1024:2048].reshape(32, 32)
+            rgbArray[..., 2] = array[2048:3072].reshape(32, 32)
+            img = im.fromarray(rgbArray)
+            img.save(path+newDF.iloc[arr]["image_name"])
+
         df=pd.concat([df,newDF])
+
     return df
 
 
 def write_df_to_csv(df):
-    df.to_csv("C:\\Users\\User\\Downloads\\cifar10.csv")
-df=create_df()
-write_df_to_csv(df)
+    df.to_csv("..\\cifar10.csv")
 
-csv=pd.read_csv("C:\\Users\\User\\Downloads\\cifar10.csv")
+
+
+df=create_df_and_images(10)
+write_df_to_csv(df)
+csv=pd.read_csv("..\\cifar10.csv")
 print(csv)
